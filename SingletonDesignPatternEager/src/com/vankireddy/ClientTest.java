661281ba38abf991052e9fdf16e149eabb81c11c
ssaiprat <saipratapysr@gmail.com>
2020-05-11 15:23:08 +0530

    first time commit

---
+++ SingletonDesignPatternEager/src/com/vankireddy/ClientTest.java
@@ -0,0 +1,14 @@
+package com.vankireddy;
+
+public class ClientTest 
+{
+	public static void main(String[] args) {
+		Singleton singleton1 = Singleton.getInstance();
+		Singleton singleton2 = Singleton.getInstance();
+	
+	System.out.println(singleton1.hashCode());
+	System.out.println(singleton2.hashCode());
+	
+	}
+
+}
